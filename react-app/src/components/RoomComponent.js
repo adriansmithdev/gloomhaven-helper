5b36bacc6ef645034c43212bbb5cc80f17218d62
Adrian Smith <ajsmith5796@gmail.com>
2019-02-01 13:47:10 -0800

    Added input for scenario number

--- react-app/src/components/RoomComponent.js
+++ react-app/src/components/RoomComponent.js
@@ -1,22 +1,45 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import MonsterList from './MonsterList';
+import axios from 'axios';
+
 
 class RoomComponent extends Component {
-  render() {
-    
-    return (
-      <div>
-        <h1>Gloomhaven Room # { this.props.match.params.roomid } </h1>
 
-        <MonsterList />
+    constructor(props){
+        super(props);
 
-        <span className="input-group-btn">
+        this.changeScenario = this.changeScenario.bind(this);
+    }
+
+    changeScenario(event) {
+        console.log("changeScenario called");
+        axios.post(`http://localhost:5000/api/room/${this.props.match.params.roomid}/scenario/${event.target.value}` )
+            .then(function (response){
+                console.log(response.data.scenarioNumber);
+            })
+            .catch(function (error){
+                console.log(error);
+            });
+    }
+
+    render() {
+
+        return (
+            <div>
+                <h1>Gloomhaven Room # {this.props.match.params.roomid} </h1>
+                <input type="number" onBlur={this.changeScenario} min="1" max="150"/>
+
+            <MonsterList/>
+
+            <span className="input-group-btn">
             <Link className="button is-dark is-large themed-font m-2" to={`/`}>Back to home!</Link>
         </span>
-      </div>
-    );
-  }
+            </div>
+        );
+    }
+
+
 }
 
 export default RoomComponent;
