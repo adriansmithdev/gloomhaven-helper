26f5ac548d02edd2e672701dd8cfe7a60b27c248
Aaron Melhaff <nash_melhaff@hotmail.com>
2019-02-27 20:25:09 -0800

    Changed text saying room to session

--- react-app/src/store/actions/session.js
+++ react-app/src/store/actions/session.js
@@ -7,12 +7,12 @@ export const getSession = function(hash) {
     const response = await axios.get(`http://localhost:5000/api/sessions?hash=${hash}`)
     .catch((error) => {
       dispatch(addError(error.response.data));
-      dispatch(setStatus('ROOM_NOT_FOUND'));
-      toast.error('That room doesn\'t exist, please enter a valid room');
+      dispatch(setStatus('SESSION_NOT_FOUND'));
+      toast.error('That session doesn\'t exist, please enter a valid session');
       return error.response.data;
     });
     dispatch(setSession(response.data.content[0]));
-    dispatch(setStatus('ROOM_FOUND'));
+    dispatch(setStatus('SESSION_FOUND'));
     return response.data;
   }
 
