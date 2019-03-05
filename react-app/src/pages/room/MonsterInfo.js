24f876b12d4d97b057107a1b7dccb2ec108b11cf
Aaron Melhaff <nash_melhaff@hotmail.com>
2019-03-04 19:35:18 -0800

    Replacing text labels with Icons

--- react-app/src/pages/room/MonsterInfo.js
+++ react-app/src/pages/room/MonsterInfo.js
@@ -1,5 +1,9 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import Attack from './../../assets/icons/stats/attack.svg';
+import Movement from './../../assets/icons/stats/movement.svg';
+import Range from './../../assets/icons/stats/range.svg';
+
 
 class MonsterInfo extends Component {
 
@@ -9,19 +13,21 @@ class MonsterInfo extends Component {
       <>
         {console.log("MONSTER")}
         {console.log(monster)}
-        <div className="columns has-text-light">
+        <div className="row has-text-light">
           <div className="title themed-font has-text-light is-size-4 column">
             {monster.name}
           </div>
           <div className="column">
-            Attack: {monster.attack}
+            <img src={Attack} className="icon-small" /> {monster.attack}
           </div>
           <div className="column">
-            Movement: {monster.movement}
+            <img src={Movement} className="icon-small" /> {monster.movement}
           </div>
           <div className="column">
-            Range: {monster.range}
+            <img src={Range} className="icon-small" /> {monster.range}
           </div>
+          
+          
           <div className="column">
             {monster.attributes !== undefined ? "Traits: " + monster.attributes : ''}
           </div>
