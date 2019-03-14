d4697b3b568aca854565075fea0b703812258d69
Aaron Melhaff <nash_melhaff@hotmail.com>
2019-03-13 21:48:41 -0700

    Restyled elements

--- react-app/src/pages/room/elements/Element.js
+++ react-app/src/pages/room/elements/Element.js
@@ -4,18 +4,31 @@ import { connect } from 'react-redux';
 const Element = (props) => {
   // Convert the fill status to array index.
   const fillStrength = [
-    '0px', '18px', '36px'
+    6, 18, 36
   ];
 
+  const elementColor = {
+      wind: '#98b0b5',
+      fire: '#e2421f',
+      earth: '#7da82a',
+      ice: '#56c8ef',
+      light: '#eca610',
+      dark: '#202830'
+  }
+
+  const containerStyles = {
+    border: `2px solid ${elementColor[props.element.name]}aa`
+  }
+
   const barStyles = {
-    backgroundColor: '#dddddd',
-    height: fillStrength[props.element.strength]
+    backgroundColor: elementColor[props.element.name],
+    height: (fillStrength[props.element.strength] - 3)
   }
 
   return (
-    <div className="element">  
+    <div style={containerStyles} className="element">  
       <div style={barStyles}></div>     
-      <img src={require(`./../../../assets/icons/elements/${props.element.name}.svg`)} />
+      <img src={require(`./../../../assets/icons/elements/${props.element.name}.svg`)} alt={props.element.name} />
        
     </div>
   );
