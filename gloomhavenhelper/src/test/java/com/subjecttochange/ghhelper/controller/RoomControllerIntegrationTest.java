5305d30059daa837ab5d10635b1534e8df648456
Kyle <Kyl.johnson@gmail.com>
2019-04-01 19:17:45 -0700

    Room controller integration tests

--- gloomhavenhelper/src/test/java/com/subjecttochange/ghhelper/controller/RoomControllerIntegrationTest.java
+++ gloomhavenhelper/src/test/java/com/subjecttochange/ghhelper/controller/RoomControllerIntegrationTest.java
@@ -1,10 +1,16 @@
 package com.subjecttochange.ghhelper.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.JsonPath;
 import com.subjecttochange.ghhelper.Application;
+import com.subjecttochange.ghhelper.exception.ResourceNotFoundException;
 import com.subjecttochange.ghhelper.persistence.model.orm.Room;
 import com.subjecttochange.ghhelper.persistence.repository.RoomRepository;
 import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,12 +21,11 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.hasItem;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SuppressWarnings("ALL")
@@ -35,21 +40,96 @@ public class RoomControllerIntegrationTest {
 
     @Autowired
     private RoomRepository roomRepository;
+    Room room;
+
+    @Before
+    public void setup() {
+        roomRepository.save(Room.createWithHash("AABBCC"));
+    }
+
+    @After
+    public void teardown() {
+        Room dirtyRoom = roomRepository.findByHash("AABBCC").orElseThrow(() -> new ResourceNotFoundException());
+        roomRepository.delete(dirtyRoom);
+    }
 
     @Test
-    public void givenRooms_getRoom_thenStatus200() throws Exception {
+    public void getRoom_jsonAllRooms() throws Exception {
+        Room room = roomRepository.findByHash("AABBCC")
+                .orElseThrow(() -> new ResourceNotFoundException());
 
-        Room room = Room.createWithHash("AABBCC");
-        roomRepository.save(room);
-
-        ResultActions actions = mvc.perform(get("/rooms")
-                .contentType(MediaType.APPLICATION_JSON))
+        mvc.perform(
+                get("/rooms")
+                    .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.content[*].hash", hasItem("AABBCC")))
-                .andExpect(jsonPath("$.content[*].scenario", is("0")))
-                .andExpect(jsonPath("$.content[*].round", is("0")));
-
+                .andExpect(jsonPath("$.content[*].scenario", hasItem(0)))
+                .andExpect(jsonPath("$.content[*].round", hasItem(0)));
     }
 
+    @Test
+    public void getRoom_jsonRoom() throws Exception {
+        Room room = roomRepository.findByHash("AABBCC")
+                .orElseThrow(() -> new ResourceNotFoundException());
+
+        mvc.perform(
+                get("/rooms")
+                        .param("hash", room.getHash())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.content[0].hash", is("AABBCC")))
+                .andExpect(jsonPath("$.content[0].scenario", is(0)))
+                .andExpect(jsonPath("$.content[0].round", is(0)));
+    }
+
+    @Test
+    public void createRoom_json() throws Exception {
+        mvc.perform(
+                post("/rooms")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.hash", notNullValue()))
+                .andExpect(jsonPath("$.scenario", is(0)))
+                .andExpect(jsonPath("$.round", is(0)));
+    }
+
+    @Test
+    public void updateRoom_jsonBody() throws Exception {
+        Room room = roomRepository.findByHash("AABBCC")
+                .orElseThrow(() -> new ResourceNotFoundException());
+        Room request = Room.createRoom("CCBBAA", 5, 10);
+        String jsonBody = new ObjectMapper().writeValueAsString(request);
+
+        mvc.perform(
+                put("/rooms")
+                        .param("hash", room.getHash())
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.hash", is("CCBBAA")))
+                .andExpect(jsonPath("$.scenario", is(5)))
+                .andExpect(jsonPath("$.round", is(10)));
+
+        // cleanup for teardown
+        room.setHash("AABBCC");
+        roomRepository.save(room);
+    }
+
+    @Test
+    public void deleteRoom_status200() throws Exception {
+        Room room = roomRepository.findByHash("AABBCC")
+                .orElseThrow(() -> new ResourceNotFoundException());
+
+        mvc.perform(
+                delete("/rooms")
+                        .param("hash", room.getHash())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        roomRepository.save(room);
+    }
 }
