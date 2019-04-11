eedfa20bcd43cee6dfbe8968dd2cd6a7ac763323
AaronMelhaff <booglessmith@gmail.com>
2019-04-11 14:03:05 -0700

    Beginning tests on element component.

---
+++ react-app/src/pages/room/elements/Element.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { shallow, configure, mount, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+
+import Element from './Element';
+
+configure({ adapter: new Adapter() });
+
+describe('Testing <Element />:', function() {
+  it('should render without throwing an error', function() {
+    expect(render(<Element type="fire"/>));
+  });
+});
