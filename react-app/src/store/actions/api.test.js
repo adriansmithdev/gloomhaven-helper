a0782e605c595431865c8773e1913555756457e8
Aaron Melhaff <nash_melhaff@hotmail.com>
2019-04-16 15:26:44 -0700

    Added monster health update test. starting element

--- react-app/src/store/actions/api.test.js
+++ react-app/src/store/actions/api.test.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { updateMonster } from './actions';
 
 
 
@@ -116,7 +117,7 @@ describe('Test API functionality:', () => {
 
     //Randomize values to be updated with
     const newScenarioNumber = Math.round(Math.random() * 10000);
-    const newScenarioLevel = Math.round(Math.random() * 10000);
+    const newScenarioLevel = Math.round(Math.random() * 7);
     const newRound = Math.round(Math.random() * 10000);
   
     //New room object to be updated
@@ -151,36 +152,134 @@ describe('Test API functionality:', () => {
   // Test Create/Delete Monsters Number.
   it('Create/Delete Monsters', async () => {
     
-    //Get Room object
-  const response = await axios.get('http://localhost:5000/api/sessions?hash=ABCDEF');
+   //Get Room object
+   const response = await axios.get('http://localhost:5000/api/sessions?hash=ABCDEF');
 
-  const monsters = response.data.content[0].monsters;
-  const randomMonster = Math.round(Math.random() * monsters.length)
-  const monsterId = monsters[randomMonster].id;
-  const instanceCount = monsters[randomMonster].monsterInstances.length;
-
-  
-  //Create normal monster
-  const normalResponse = await axios.post(`http://localhost:5000/api/monsterinstances?hash=ABCDEF`, {
+   const monsters = response.data.content[0].monsters;
+   const randomMonster = Math.round(Math.random() * monsters.length)
+   const monsterId = monsters[randomMonster].id;
+   const instanceCount = monsters[randomMonster].monsterInstances.length;
+ 
+   
+   //Create normal monster
+   const normalResponse = await axios.post(`http://localhost:5000/api/monsterinstances?hash=ABCDEF`, {
       monsterId: monsterId,
       isElite: false
-  });
+   });
   
-  //Create elite monster
-  const eliteResponse = await axios.post(`http://localhost:5000/api/monsterinstances?hash=ABCDEF`, {
+   //Create elite monster
+   const eliteResponse = await axios.post(`http://localhost:5000/api/monsterinstances?hash=ABCDEF`, {
+       monsterId: monsterId,
+       isElite: true
+   });
+    
+   const updatedResponse = await axios.get('http://localhost:5000/api/sessions?hash=ABCDEF');
+   const updatedMonsters = updatedResponse.data.content[0].monsters;
+   const updatedInstanceCount = updatedMonsters[randomMonster].monsterInstances.length;
+
+   expect(updatedInstanceCount).not.toBe(undefined);
+   expect(updatedInstanceCount).not.toBe(instanceCount);
+   expect(updatedInstanceCount).toBe(instanceCount + 2);
+  
+  });
+
+
+  // Test updating Health.
+  it('Testing Health Functions Calls', async () => {
+
+    //Get Room object
+    const response = await axios.get('http://localhost:5000/api/sessions?hash=ABCDEF');
+
+    const monsters = response.data.content[0].monsters;
+    const randomMonster = Math.round(Math.random() * monsters.length)
+    const monsterId = monsters[randomMonster].id;
+    const startingHealth = monsters[randomMonster].maxHealth;
+
+    //Create normal monster
+    const normalResponse = await axios.post(`http://localhost:5000/api/monsterinstances?hash=ABCDEF`, {
       monsterId: monsterId,
-      isElite: true
-  });
-   
-  const updatedResponse = await axios.get('http://localhost:5000/api/sessions?hash=ABCDEF');
-  const updatedMonsters = updatedResponse.data.content[0].monsters;
-  const updatedInstanceCount = updatedMonsters[randomMonster].monsterInstances.length;
+      isElite: false
+    });
 
-  expect(updatedInstanceCount).not.toBe(undefined);
-  expect(updatedInstanceCount).not.toBe(instanceCount);
-  expect(updatedInstanceCount).toBe(instanceCount + 2);
+    const newInstance = normalResponse.data;
+    const instanceId = newInstance.id;
+
+    const newHealth = newInstance.currentHealth - 1;
+
+    const updateInstance = {
+      ...newInstance,
+      currentHealth: newHealth
+    };
+    
+    const updateResult = await axios.put(`http://localhost:5000/api/monsterinstances?hash=ABCDEF&id=${newInstance.id}`, updateInstance);
+
+    const updatedMonster = updateResult.data;
+    expect(updatedMonster.currentHealth).not.toBe(undefined);
+    expect(updatedMonster.currentHealth).not.toBe(newInstance.currentHealth);
+    expect(updatedMonster.currentHealth).toBe(newHealth);
+
+  });
+
+
+  // Test Elements updating with rounds.
+  it('Elements update with round', async () => {
+
+    //Get Room object
+    const response = await axios.get('http://localhost:5000/api/sessions?hash=ABCDEF');
+
+    //get element to be checked
+    const elements = response.data.content[0].room.elements
+    const randomElement = Math.round(Math.random() * elements.length)
+    const element = elements[randomElement];
+
+    const updateElement = {
+      ...element,
+      strength: newStrength
+    };
+
+    expect(element).not.toBe(undefined);
+    expect(element.state).toBe(2);
+
+    //push to database
+    await axios.put(`http://localhost:5000/api/elements?hash=ABCDEF&id=${element.id}`, element);
+    
+    const updatedResponseOne = await axios.get('http://localhost:5000/api/sessions?hash=ABCDEF');
+
+    //get element to be checked
+    const updatedElementsOne = updatedResponseOne.data.content[0].elements
+    const updatedElementOne = updatedElementsOne[randomElement];
+
+    //Test to element was pushed correctly
+    expect(updatedElementOne).not.toBe(undefined);
+    //Check to make sure the state is in max
+    expect(updatedElementOne.state).toBe(2);
+    
+    const newRound = updatedResponseOne.data.content[0].room.round++;
+
+    //New room object to be updated
+    const newRoom = {
+      ...room, 
+      scenarioNumber: 1,
+      scenarioLevel: 0,
+      round: newRound
+    }
+
+    //Update Room
+    await axios.put(`http://localhost:5000/api/rooms?hash=ABCDEF`, newRoom);
+
+    //Get Updated Room
+    const updatedResponseTwo = await axios.get('http://localhost:5000/api/sessions?hash=ABCDEF');
+    //get element to be checked if decreased with round increment
+    const updatedElementsTwo = updatedResponseTwo.data.content[0].elements
+    const updatedElementTwo = updatedElementsTwo[randomElement];
+
+
+    //Test to element was pushed correctly
+    expect(updatedElementTwo).not.toBe(undefined);
+    //Check to make sure the state is in max
+    expect(updatedElementTwo.state).toBe(1);
+
+  });
   
-  });
-
 
 });
