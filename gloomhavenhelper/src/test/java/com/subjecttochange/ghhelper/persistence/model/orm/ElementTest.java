5bfc6f88139f8ee4fe114d98c8c922fbfd345a23
Tony <tony.g.thompson@gmail.com>
2019-04-16 14:17:42 -0700

    Added tests for Stat and Element classes

--- gloomhavenhelper/src/test/java/com/subjecttochange/ghhelper/persistence/model/orm/ElementTest.java
+++ gloomhavenhelper/src/test/java/com/subjecttochange/ghhelper/persistence/model/orm/ElementTest.java
@@ -1,6 +1,5 @@
 package com.subjecttochange.ghhelper.persistence.model.orm;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
@@ -13,48 +12,64 @@ import static org.junit.Assert.*;
 @ActiveProfiles("test")
 public class ElementTest {
 
-    public static final int ZERO = 0;
+    private static final int ZERO = 0;
+    private static final int ONE = 1;
+    private static final int TWO = 2;
+    private static final int SIX = 6;
     private List<Element> elements = new ArrayList<>();
     private Room room = new Room();
 
     @Before
-    public void setUp() throws Exception {
-        elements = Element.createElementsForRoom(0, room);
+    public void setUp() {
+        elements = Element.createElementsForRoom(ZERO, room);
         room.setElements(elements);
     }
 
 
-    @After
-    public void tearDown() throws Exception {
+    @Test
+    public void createElementsForRoom() {
+        List<Element> testElements = Element.createElementsForRoom(ZERO, room);
+        assertEquals(
+                "Size should be 6",
+                SIX,
+                testElements.size()
+        );
     }
 
 
     @Test
     public void decrementElementsByQuantity() {
-        elements.get(0).setStrength(2);
-        Element.decrementElementsByQuantity(room, 1);
-        int strength = elements.get(0).getStrength();
+        elements.get(0).setStrength(TWO);
+        Element.decrementElementsByQuantity(room, ONE);
         assertEquals(
-                "Strength should be 1",
-                1,
-                strength
+                "Strength should be reduced from 2 to 1",
+                ONE,
+                firstElementStrength()
         );
     }
 
 
     @Test
     public void updateElement() {
-
+        Element request = Element.createElement(Element.ElementType.FIRE, TWO, room);
+        elements.get(0).updateElement(request);
+        assertEquals(
+                "Strength should be 2",
+                TWO,
+                firstElementStrength());
     }
 
 
     @Test
     public void getStrength() {
-        int strength = elements.get(0).getStrength();
         assertEquals(
                 "Strength should be 0",
                 ZERO,
-                strength
+                firstElementStrength()
         );
+    }
+
+    private int firstElementStrength() {
+        return elements.get(0).getStrength();
     }
 }
