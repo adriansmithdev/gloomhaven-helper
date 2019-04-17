9db3f7a98c7cb5aa8af4e1e148572fd82194cde1
Kyle <Kyl.johnson@gmail.com>
2019-04-16 21:44:48 -0700

    Stat and status tests

--- gloomhavenhelper/src/test/java/com/subjecttochange/ghhelper/controller/StatControllerTest.java
+++ gloomhavenhelper/src/test/java/com/subjecttochange/ghhelper/controller/StatControllerTest.java
@@ -1,17 +1,29 @@
 package com.subjecttochange.ghhelper.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.subjecttochange.ghhelper.Application;
+import com.subjecttochange.ghhelper.persistence.model.orm.Element;
+import com.subjecttochange.ghhelper.persistence.model.orm.Stat;
+import com.subjecttochange.ghhelper.persistence.service.StatService;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SuppressWarnings("ALL")
 @RunWith(SpringRunner.class)
@@ -21,27 +33,80 @@ import static org.junit.Assert.*;
 @ActiveProfiles("test")
 public class StatControllerTest {
 
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private StatService statService;
+    private Stat stat;
+
     @Before
     public void setUp() throws Exception {
+        stat = stat.create("Strength", "Big strong arms");
+        stat = statService.createStat(stat);
     }
 
     @After
     public void tearDown() throws Exception {
+        statService.deleteStat(stat.getId());
     }
 
     @Test
-    public void getStat() {
+    public void getStat() throws Exception {
+        mvc.perform(
+                get("/stats")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.content[0].id", notNullValue()))
+                .andExpect(jsonPath("$.content[0].name", is(stat.getName())))
+                .andExpect(jsonPath("$.content[0].tooltip", is(stat.getTooltip())));
     }
 
     @Test
-    public void createStat() {
+    public void createStat() throws Exception {
+        String jsonBody = new ObjectMapper().writeValueAsString(stat);
+
+        MvcResult result = mvc.perform(
+                post("/stats")
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", notNullValue()))
+                .andExpect(jsonPath("$.name", is(stat.getName())))
+                .andExpect(jsonPath("$.tooltip", is(stat.getTooltip())))
+                .andReturn();
+
+        String resultString = result.getResponse().getContentAsString();
+        Stat createdStat = new ObjectMapper().readValue(resultString, Stat.class);
+        statService.deleteStat(createdStat.getId());
     }
 
     @Test
-    public void updateStat() {
+    public void updateStat() throws Exception {
+        Stat request = Stat.create("Weak", "Small arms");
+        String jsonBody = new ObjectMapper().writeValueAsString(request);
+
+        mvc.perform(
+                put("/stats")
+                        .param("id", stat.getId().toString())
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", notNullValue()))
+                .andExpect(jsonPath("$.name", is(request.getName())))
+                .andExpect(jsonPath("$.tooltip", is(request.getTooltip())));
     }
 
     @Test
-    public void deleteStat() {
+    public void deleteStat() throws Exception {
+        mvc.perform(
+                delete("/stats")
+                        .param("id", stat.getId().toString())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        stat = statService.createStat(stat);
     }
 }
--- gloomhavenhelper/src/test/java/com/subjecttochange/ghhelper/controller/StatusControllerTest.java
+++ gloomhavenhelper/src/test/java/com/subjecttochange/ghhelper/controller/StatusControllerTest.java
@@ -1,17 +1,29 @@
 package com.subjecttochange.ghhelper.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.subjecttochange.ghhelper.Application;
+import com.subjecttochange.ghhelper.persistence.model.orm.Stat;
+import com.subjecttochange.ghhelper.persistence.model.orm.monster.Status;
+import com.subjecttochange.ghhelper.persistence.service.StatusService;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SuppressWarnings("ALL")
 @RunWith(SpringRunner.class)
@@ -21,27 +33,81 @@ import static org.junit.Assert.*;
 @ActiveProfiles("test")
 public class StatusControllerTest {
 
+
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private StatusService statusService;
+    private Status status;
+
     @Before
     public void setUp() throws Exception {
+        status = status.create("Bleed", "Take damage");
+        status = statusService.createStatus(status);
     }
 
     @After
     public void tearDown() throws Exception {
+        statusService.deleteStatus(status.getId());
     }
 
     @Test
-    public void getStatus() {
+    public void getStatus() throws Exception {
+        mvc.perform(
+                get("/statuses")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.content[0].id", notNullValue()))
+                .andExpect(jsonPath("$.content[0].name", is(status.getName())))
+                .andExpect(jsonPath("$.content[0].tooltip", is(status.getTooltip())));
     }
 
     @Test
-    public void createStatus() {
+    public void createStatus() throws Exception {
+        String jsonBody = new ObjectMapper().writeValueAsString(status);
+
+        MvcResult result = mvc.perform(
+                post("/statuses")
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", notNullValue()))
+                .andExpect(jsonPath("$.name", is(status.getName())))
+                .andExpect(jsonPath("$.tooltip", is(status.getTooltip())))
+                .andReturn();
+
+        String resultString = result.getResponse().getContentAsString();
+        Status createdStaus = new ObjectMapper().readValue(resultString, Status.class);
+        statusService.deleteStatus(createdStaus.getId());
     }
 
     @Test
-    public void updateStatus() {
+    public void updateStatus() throws Exception {
+        Status request = Status.create("Stunned", "Cannot move");
+        String jsonBody = new ObjectMapper().writeValueAsString(request);
+
+        mvc.perform(
+                put("/statuses")
+                        .param("id", status.getId().toString())
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", notNullValue()))
+                .andExpect(jsonPath("$.name", is(request.getName())))
+                .andExpect(jsonPath("$.tooltip", is(request.getTooltip())));
     }
 
     @Test
-    public void deleteStatus() {
+    public void deleteStatus() throws Exception {
+        mvc.perform(
+                delete("/statuses")
+                        .param("id", status.getId().toString())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        status = statusService.createStatus(status);
     }
 }
