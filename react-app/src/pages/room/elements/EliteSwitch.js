02dc3088416cc51592ac4e3fa2a11f3f9f976a23
Aaron Melhaff <nash_melhaff@hotmail.com>
2019-04-17 01:29:05 -0700

    Removed unused redux connection

--- react-app/src/pages/room/elements/EliteSwitch.js
+++ react-app/src/pages/room/elements/EliteSwitch.js
@@ -1,33 +1,19 @@
 import React, {Component} from 'react';
-import { connect } from 'react-redux';
 
 import './switch.scss';
 
 class EliteSwitch extends Component {
 
     render(){ 
-
-
-        return (
-            <div className="">
-                <span className="themed-font has-text-white subtitle is-3 toggle-button-label">Elite: </span>
-                <input className="toggle" type="checkbox" onClick={ this.props.updateEliteStatus } />
-            </div>
-        );
-
+      return (
+        <div className="">
+          <span className="themed-font has-text-white subtitle is-3 toggle-button-label">Elite: </span>
+          <input className="toggle" type="checkbox" onClick={ this.props.updateEliteStatus } />
+        </div>
+      );
     }
     
 }
 
-const mapStateToProps = (state) => {
-    return {};
-}
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        //UI event functions
-    };
-}
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(EliteSwitch);
+export default EliteSwitch;
