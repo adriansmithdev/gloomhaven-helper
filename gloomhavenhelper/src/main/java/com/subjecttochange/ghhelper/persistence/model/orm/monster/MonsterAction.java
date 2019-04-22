f4115818f3bbfb4df9f01d8a7ea882cf9bf3f16d
Tony <tony.g.thompson@gmail.com>
2019-04-22 12:07:27 -0700

    Monster Action object created

---
+++ gloomhavenhelper/src/main/java/com/subjecttochange/ghhelper/persistence/model/orm/monster/MonsterAction.java
@@ -0,0 +1,10 @@
+package com.subjecttochange.ghhelper.persistence.model.orm.monster;
+
+import java.util.Set;
+
+public class MonsterAction {
+
+    private boolean shuffle;
+    private Integer initiative;
+    private Set<String> actionDeck;
+}
