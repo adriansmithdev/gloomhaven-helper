b0c39e55dc8cb662ea62c40fb42eb57dbc7b8c13
Aaron Melhaff <nash_melhaff@hotmail.com>
2019-04-23 15:31:57 -0700

    Added temporary widget for elite toggle.

---
+++ react-app/src/pages/common/EliteToggle.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './EliteToggle.scss';
+
+const EliteToggle = (props) => {
+  
+  const isActive = (props.toggleStatus === true)
+
+  const buttonText = (isActive) ? ' Elite' : 'Normal';
+  const classes = (isActive) ? 'button is-black elite-toggle themed-font active' : 'button is-dark elite-toggle themed-font';
+  return (
+    <button className={classes}
+      onClick={props.onClick}>
+      {buttonText}
+    </button>
+  );
+    
+}
+
+
+export default EliteToggle;
