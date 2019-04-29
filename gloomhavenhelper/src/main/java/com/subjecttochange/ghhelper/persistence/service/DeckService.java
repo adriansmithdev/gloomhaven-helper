b8bb7675d7c5a787d5a444f237591d2adc625cbf
Kyle <Kyl.johnson@gmail.com>
2019-04-29 02:00:48 -0700

    Draw monster action lazy save

--- gloomhavenhelper/src/main/java/com/subjecttochange/ghhelper/persistence/service/DeckService.java
+++ gloomhavenhelper/src/main/java/com/subjecttochange/ghhelper/persistence/service/DeckService.java
@@ -1,12 +1,12 @@
 package com.subjecttochange.ghhelper.persistence.service;
 
+import com.subjecttochange.ghhelper.persistence.model.orm.Room;
 import com.subjecttochange.ghhelper.persistence.model.orm.monster.Monster;
 import com.subjecttochange.ghhelper.persistence.model.orm.monster.MonsterAction;
 import com.subjecttochange.ghhelper.persistence.model.orm.monster.MonsterActionDeck;
 import com.subjecttochange.ghhelper.persistence.repository.DeckRepository;
 import com.subjecttochange.ghhelper.persistence.repository.MonsterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -20,22 +20,34 @@ public class DeckService {
     private final MonsterRepository monsterRepository;
 
     @Autowired
-    public DeckService(DeckRepository deckRepository, MonsterRepository monsterRepository){
+    public DeckService(DeckRepository deckRepository, MonsterRepository monsterRepository) {
         this.deckRepository = deckRepository;
         this.monsterRepository = monsterRepository;
     }
 
-    public List<MonsterActionDeck> getMonsterActionDecks(List<Monster> monsters){
+    public List<MonsterActionDeck> getMonsterActionDecks(List<Monster> monsters) {
         List<String> monsterNames = new ArrayList<>();
 
-        for(Monster monster: monsters){
+        for (Monster monster : monsters) {
             monsterNames.add(monster.getName());
         }
 
         return deckRepository.findAllByMonsterName(monsterNames);
     }
 
-    public Map<Monster, MonsterAction> drawMonsterActions(String hash, List<Monster> monsters){
+    public Map<Monster, MonsterAction> drawMonsterActions(Room room, List<Monster> monsters) {
+        Map<Monster, MonsterActionDeck> roomDecks = room.getDecks();
+
+        for (Monster monster : monsters) {
+            if (!roomDecks.containsKey(monster)) {
+                MonsterActionDeck seededDeck = deckRepository.findByMonsterName(monster.getName());
+                roomDecks.put(monster, MonsterActionDeck.createDeck(monster.getName(), seededDeck.getActionDeck()));
+            }
+        }
+
+        for (MonsterActionDeck monsterActionDeck : roomDecks.values()) {
+            monsterActionDeck.drawNewMonsterAction();
+        }
 
         return null;
     }
