dc3288810643432311075dd81b7aaebf8ec61cfc
Kyle <Kyl.johnson@gmail.com>
2019-05-08 11:28:35 -0700

    Collaborating on web sockets

---
+++ gloomhavenhelper/src/main/java/com/subjecttochange/ghhelper/controller/EventController.java
@@ -0,0 +1,34 @@
+package com.subjecttochange.ghhelper.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.time.LocalTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Controller
+public class EventController {
+
+    @GetMapping("/stream-sse-mvc")
+    public SseEmitter streamSseMvc() {
+        SseEmitter emitter = new SseEmitter();
+        ExecutorService sseMvcExecutor = Executors.newSingleThreadExecutor();
+        sseMvcExecutor.execute(() -> {
+            try {
+                for (int i = 0; true; i++) {
+                    SseEmitter.SseEventBuilder event = SseEmitter.event()
+                            .data("SSE MVC - " + LocalTime.now().toString())
+                            .id(String.valueOf(i))
+                            .name("sse event - mvc");
+                    emitter.send(event);
+                    Thread.sleep(1000);
+                }
+            } catch (Exception ex) {
+                emitter.completeWithError(ex);
+            }
+        });
+        return emitter;
+    }
+}
