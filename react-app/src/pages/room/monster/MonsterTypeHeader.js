9e5419f255671a60b4955cfd552792ae2503aad8
Aaron Melhaff <nash_melhaff@hotmail.com>
2019-05-22 04:05:53 -0700

    Refactored to use icon manager for stats

--- react-app/src/pages/room/monster/MonsterTypeHeader.js
+++ react-app/src/pages/room/monster/MonsterTypeHeader.js
@@ -3,11 +3,6 @@ import React, {Component} from 'react';
 import MonsterAttribute from './MonsterAttribute';
 import IconManager from './../../common/IconManager';
 
-import Attack    from './../../../assets/icons/stats/attack.svg';
-import Movement  from './../../../assets/icons/stats/movement.svg';
-import Range     from './../../../assets/icons/stats/range.svg';
-
-
 class MonsterTypeHeader extends Component {
 
   constructor(props) {
@@ -43,6 +38,11 @@ class MonsterTypeHeader extends Component {
   render() {
     let monster = this.props.type;
     let portrait;
+
+    const attack = IconManager('%attack%');
+    const move   = IconManager('%move%');
+    const range  = IconManager('%range%');
+
     try {
       portrait = require(`./../../../assets/portraits/monsters/mini/${monster.name}.png`);
     } catch(e) {
@@ -60,21 +60,21 @@ class MonsterTypeHeader extends Component {
         </div>
         <div className="monster-stats">
           <div className="monster-stat" title="Base damage dealt by monsters with each attack.">
-            <img src={Attack} className="inline-icon" alt="Attack: " />
+            <img src={attack.src} className="inline-icon" alt={attack.alt} />
             <div>
               <p className="monster-elite-stat">{monster.eliteAttack}</p>
               <p className="monster-normal-stat">{monster.attack}</p>
             </div>
           </div>
           <div className="monster-stat" title="Number of tiles the monsters is allowed to move.">
-            <img src={Movement} className="inline-icon" alt="Movement: " />
+            <img src={move.src} className="inline-icon" alt={move.alt} />
             <div>
               <p className="monster-elite-stat">{monster.eliteMove}</p>
               <p className="monster-normal-stat">{monster.movement}</p>
             </div>
           </div>
           <div className="monster-stat" title="Number of tiles that the monsters can attack from">
-            <img src={Range} className="inline-icon" alt="Range: " />
+            <img src={range.src} className="inline-icon" alt={range.alt} />
             <div>
               <p className="monster-elite-stat">{monster.eliteRange}</p>
               <p className="monster-normal-stat">{monster.range}</p>
