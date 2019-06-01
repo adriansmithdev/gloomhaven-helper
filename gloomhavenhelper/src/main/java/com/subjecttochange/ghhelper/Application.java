4016076bc09eaeb59d15ce65f35db77f21b8a91e
Kyle <Kyl.johnson@gmail.com>
2019-05-31 20:15:23 -0700

    SSL in springboot

--- gloomhavenhelper/src/main/java/com/subjecttochange/ghhelper/Application.java
+++ gloomhavenhelper/src/main/java/com/subjecttochange/ghhelper/Application.java
@@ -1,5 +1,6 @@
 package com.subjecttochange.ghhelper;
 
+import org.apache.coyote.AbstractProtocol;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -26,33 +27,11 @@ import java.util.Arrays;
 @EntityScan("com.subjecttochange.ghhelper.*")
 public class Application {
 
-    private static final boolean PRINTING_BEANS = false;
-
     /**
      * @param args parameters to start application with
      */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    /**
-     * Runs before the application starts
-     * @param context of the application
-     * @return inspection of project beans
-     */
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext context) {
-        return args -> {
-            if (PRINTING_BEANS) {
-                System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-                String[] beanNames = context.getBeanDefinitionNames();
-                Arrays.sort(beanNames);
-                for (String beanName : beanNames) {
-                    System.out.println(beanName);
-                }
-            }
-        };
     }
 
 }
---
+++ gloomhavenhelper/src/main/java/com/subjecttochange/ghhelper/configuration/HttpsConfiguration.java
@@ -0,0 +1,70 @@
+package com.subjecttochange.ghhelper.configuration;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.File;
+import java.io.IOException;
+
+@Configuration
+public class HttpsConfiguration {
+
+    @Value("${server.port}")
+    int httpPort;
+    @Value("${server.port.ssl}")
+    int httpsPort;
+    @Value("${server.ssl.key-store-type}")
+    String type;
+    @Value("${server.ssl.key-store-password}")
+    String password;
+    @Value("${server.ssl.key-alias}")
+    String alias;
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+
+        tomcat.addAdditionalTomcatConnectors(createSslConnector());
+        return tomcat;
+    }
+
+    private Connector createSslConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+        try {
+            File keystore = new ClassPathResource("keystore/baeldung.p12").getFile();
+            connector.setScheme("https");
+            connector.setSecure(true);
+            connector.setPort(httpsPort);
+            protocol.setSSLEnabled(true);
+            protocol.setKeystoreType(type);
+            protocol.setKeystoreFile(keystore.getAbsolutePath());
+            protocol.setKeystorePass(password);
+            protocol.setKeyAlias(alias);
+            return connector;
+        }
+        catch (IOException ex) {
+            throw new IllegalStateException("can't access keystore: [" + "keystore"
+                    + "] or truststore: [" + "keystore" + "]", ex);
+        }
+    }
+}
