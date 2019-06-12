4dcd34db9391a0194346cc75b17a43e4481b516a
Adrian Smith <ajsmith5796@gmail.com>
2019-06-12 14:12:51 -0700

    Changed API url to reflect new environment.

--- react-app/src/axios.config.js
+++ react-app/src/axios.config.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const apiURL = (process.env.NODE_ENV === 'development') ?
   'http://localhost:5000/api/' :
-  'http://gloomhaven-helper.us-west-2.elasticbeanstalk.com/api/';
+  'http://gloomtility.us-west-2.elasticbeanstalk.com/api/';
   
 export const configuredAxios = axios.create({
   baseURL: apiURL,
