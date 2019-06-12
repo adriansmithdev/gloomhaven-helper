657fd403890dd7c17221256eed2d964d165be605
AaronMelhaff <booglessmith@gmail.com>
2019-06-12 12:02:32 -0700

    Using different route on dev vs prod.

--- react-app/src/axios.config.js
+++ react-app/src/axios.config.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
-export const apiURL = 'http://localhost:5000/api/'
-
+export const apiURL = (process.env.NODE_ENV === 'development') ?
+  'http://localhost:5000/api/' :
+  'http://gloomhaven-helper.us-west-2.elasticbeanstalk.com/api/';
+  
 export const configuredAxios = axios.create({
   baseURL: apiURL,
   timeout: 4000
